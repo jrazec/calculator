4e2ad427bd2b683e6d255a2021625e4a2a34a990
jrazec <johnrazecschool@gmail.com>
2023-08-12 20:44:57 +0800

    Create 4 operation functions, operate, and 3 variables

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+function minus(num1,num2){
+    return num1 - num2;
+}
+function multiply(num1,num2){
+    return num1 * num2;
+}
+function divide(num1,num2){
+    return num1 / num2;
+}
+
+function operate(operator, firstNum,secondNumscr    ){
+
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+
