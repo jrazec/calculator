234247b4e1f1e2280905ea562ea589dcd7e4f2cf
jrazec <johnrazecschool@gmail.com>
2023-08-13 11:13:41 +0800

    Add document selectors, adjust code arrangement, add eventlistener to numbers

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let firstNum;
 let secondNum;
 let operator;
+let stringNum = "";//string
+let answer = "trial"; //this should be float
 
 const numberDisplay = document.querySelector('h1');
+const numberButtons = document.querySelectorAll('.number');//try to convert in array list
+const operatorButtons = document.querySelectorAll('.operator');
+const clearButton = document.getElementById('clear');
+const signButton = document.getElementById('sign');
+const decimalButton = document.getElementById('.');
+const equalButton = document.getElementById('=');
 
-let answer = "trial"; //this should be float
+//              Event Listeners for Buttons
+numberButtons.forEach(button=>{
+    button.addEventListener('click',() => {
+        stringNum += button.id;
+    });
+});
+
+//              Function Operators
 function add(num1,num2){
     answer = num1 + num2;
     return answer;
@@ -22,6 +37,7 @@ function divide(num1,num2){
     return answer;
 }
 
+//              Display and Operation
 function displayNumber() {
     numberDisplay.textContent = answer;
 }
@@ -31,7 +47,7 @@ function operate(operator, firstNum,secondNum){
     displayNumber();
 }
 
-
+operate(add, 2000, 100);
 
 
 
