fccf5a5c12a23dc2bc247f24477515ca991dcbb8
jrazec <johnrazecschool@gmail.com>
2023-08-13 10:38:24 +0800

    Add displayNumber and answer variable container

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-function add(num1,num2){
-    return num1 + num2;
-}
-function minus(num1,num2){
-    return num1 - num2;
-}
-function multiply(num1,num2){
-    return num1 * num2;
-}
-function divide(num1,num2){
-    return num1 / num2;
-}
-
-function operate(operator, firstNum,secondNumscr    ){
-
-}
-
 let firstNum;
 let secondNum;
 let operator;
+
+const numberDisplay = document.querySelector('h1');
+
+let answer = "trial"; //this should be float
+function add(num1,num2){
+    answer = num1 + num2;
+    return answer;
+}
+function minus(num1,num2){
+    answer = num1 - num2;
+    return answer;
+}
+function multiply(num1,num2){
+    answer = num1 * num2;
+    return answer;
+}
+function divide(num1,num2){
+    answer = num1 / num2;
+    return answer;
+}
+
+function displayNumber() {
+    numberDisplay.textContent = answer;
+}
+
+function operate(operator, firstNum,secondNum){
+    operator(firstNum,secondNum);
+    displayNumber();
+}
+
+
+
 
 
